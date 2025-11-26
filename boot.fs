c0eb4801349be0b29eea65f9cc231781dfee0a0b
Andrew McKewan <andrew.mckewan@gmail.com>
2025-11-26 10:46:38 -0500

    Implement polyFORTH vocabularies for gforth

--- boot.fs
+++ boot.fs
@@ -1,60 +1,59 @@
-\ Boot code, build with gforth
+( Initialization file for gforth )
+\ gforth boot.fs -e hi
 
-[DEFINED] EMPTY [IF] EMPTY [THEN] MARKER EMPTY
-ONLY FORTH ALSO DEFINITIONS
-WARNINGS ON
+ONLY FORTH ALSO DEFINITIONS DECIMAL
+warnings off
+
+\ : marker, ( -- mark ) \ builds marker at HERE
+\ : marker! ( mark -- ) \ restores state
+variable golden
+: GILD    align marker, golden ! ;
+: EMPTY   golden @ marker! ;
+
+\ Simulate polyFORTH-style vocabularies.
+\ CONTEXT specifies the search order, up to 4 wordlists.
+\ There are up to 8 wordlists, 1=FORTH, 3=ASSEMBLER, 5=EDITOR,
+\ 5 more for apps
+
+WORDLIST CONSTANT ASSEMBLER \ nice for order
+WORDLIST CONSTANT EDITOR
+
+WORDLIST WORDLIST WORDLIST WORDLIST WORDLIST 
+EDITOR ASSEMBLER FORTH-WORDLIST 
+CREATE WORDLISTS  , , , , , , , ,
+
+variable 'context
+variable 'current
+
+: 'wid ( n - a)  15 and 1- 2/ cells wordlists + ;
+
+: voc>order ( n)
+   dup 'context !  0 swap ( order n)
+   begin ?dup while
+      dup 12 rshift 15 and ?dup if ( order n v# )
+         swap >r  'wid @ swap 1+ ( +order )  r>
+      then  4 lshift $ffff and
+   repeat set-order ;
+
+: VOCABULARY ( n)  create , does> @ voc>order ;
+
+: DEFINITIONS   'context @ 'current !  definitions ;
+: :   :  CURRENT @ CONTEXT !  'current @ voc>order ; \ NON-STANDARD!
+
 HEX
-
-: TAG S" cross" ;
-
-: H, , ;
-
-\ Memory Access Words
-CREATE IMAGE 2000 ALLOT   IMAGE 2000 ERASE
-: >T     ( taddr -- addr )   IMAGE + ;
-: TC@    ( taddr -- char )   >T C@ ;
-: TC!    ( char taddr -- )   >T C! ;
-: T@     ( taddr -- u )      >T UW@ ;
-: T@S    ( taddr -- n )      >T SW@ ;
-: T!     ( n taddr -- )      >T W! ;
-
-VARIABLE H
-: THERE  ( -- taddr )   H @ ;
-: TALLOT ( n -- )       H +! ;
-: TC,    ( char -- )    H @ TC!   1 H +! ;
-: T,     ( n -- )       H @  T!   2 H +! ;
-: TS, ( addr len -- )   0 ?DO  COUNT TC,  LOOP DROP ;
-
-: TALIGN   HERE 1 AND IF 0 C, THEN ;
-
-: TDUMP   IMAGE H @ DUMP ;
-
-1 BLOCK DROP ( seems gforth needs a kick-start )
-: SAVE   IMAGE 0 BUFFER 400 CMOVE  UPDATE  FLUSH ;
-
-
-VOCABULARY ASSEMBLER
-ASSEMBLER DEFINITIONS
-: OP  ( op)   CREATE C, DOES> C@ TC, ;
-: OP# ( op)   CREATE C, DOES> C@ TC, T, ;
-: OPR ( op)   CREATE C, DOES> C@ TC, HERE SWAP - TC, ;
-
-00 OP NEXT      01 OP RET       02 OP# CALL     03 OP# JMP
-04 OPR BRA    05 OP NOP1      06 OP LIT       07 OP# #
-08 OP SWI
-
-\  x0 OP +         x1 OP -         x2 OP *         x3 OP /
-\  x0 OP ADD         x1 OP SUB         x2 OP MUL         x3 OP DIV
-
-FF OP DIE
+0001 VOCABULARY FORTH
+0013 VOCABULARY ASSEMBLER
+0015 VOCABULARY EDITOR
+DECIMAL
 
 FORTH DEFINITIONS
-: C, TC, ;
-: , T, ;
-ASSEMBLER
-\ u8 hello[] = { 7,11,0,  7,3,0,  7,1,0,  8,  255,  'h','i','\n'};
-B #  3 #  1 #  SWI  DIE  'h' C, 'i' C, A C,
+: HI   9 LOAD ;
 
-FORTH DEFINITIONS
+1024 CONSTANT B/BUF
+: L   SCR @ LIST ;
+: N    1 SCR +!  L ;
+: B   -1 SCR +!  L ;
 
-\ : RESET  0 BLOCK 0 B/BUF MOVE  0 >R ;
+
+warnings on
+
